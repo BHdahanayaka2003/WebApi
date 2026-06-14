0c45b70a875ee007f4364c59a0658cfc45037a92
BHdahanayaka2003 <budhvinhimsadadahanayaka@gmail.com>
2026-06-14 10:10:32 +0530

    Add package.json with express dependency

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,18 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-  res.end('Hello World');
-}).listen(3000);
+// http.createServer((req, res) => {
+//   res.end('Hello World');
+// }).listen(3000);
 
-console.log('Open http://localhost:3000');  
+// console.log('Open http://localhost:3000');  
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
